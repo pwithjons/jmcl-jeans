f95166d2d16431c15089e55db0e42f469b184e47
Jonynactar <jonynactar@gmail.com>
2026-09-15 23:25:44 +0600

    Add entrypoint script for Docker container

---
+++ jmcl-jeans/docker/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -e
+
+php artisan config:clear
+php artisan migrate --force
+php artisan storage:link || true
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+
+exec php artisan serve --host=0.0.0.0 --port="${PORT:-10000}"
