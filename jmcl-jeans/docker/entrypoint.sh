c19a11e40c0d2743c624e3cf12f2c4af3b77cd86
Jonynactar <jonynactar@gmail.com>
2026-09-16 00:52:55 +0600

    Add database seeding in entrypoint script

--- jmcl-jeans/docker/entrypoint.sh
+++ jmcl-jeans/docker/entrypoint.sh
@@ -3,9 +3,9 @@ set -e
 
 php artisan config:clear
 php artisan migrate --force
+php artisan db:seed --class=AdminSeeder --force
 php artisan storage:link || true
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
-
 exec php artisan serve --host=0.0.0.0 --port="${PORT:-10000}"
